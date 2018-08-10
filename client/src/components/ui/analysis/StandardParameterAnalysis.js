80c2393e427405c33aa795d54030656ff7509cf9
Ben Huang <bhuang@thousandeyes.com>
2018-08-10 02:31:03 -0700

    adds a title bar in param analysis

--- client/src/components/ui/analysis/StandardParameterAnalysis.js
+++ client/src/components/ui/analysis/StandardParameterAnalysis.js
@@ -8,38 +8,42 @@ import { timeParse } from "d3-time-format";
  *            val: [...]
  *        ]
  */
-const convertTwoArrToOneArray = (data) => {
-    let parseTime = timeParse("%Y-%m");
+const convertTwoArrToOneArray = data => {
+  let parseTime = timeParse("%Y-%m");
   let newArray = [];
-  for (let i=0; i<data.date.length; i++) {
-      newArray.push(data.val[i] ? {date: parseTime(data.date[i]), val: data.val[i]} : null);
+  for (let i = 0; i < data.date.length; i++) {
+    newArray.push(
+      data.val[i] ? { date: parseTime(data.date[i]), val: data.val[i] } : null
+    );
   }
   return newArray;
 };
 
 class StandardParameterAnalysis extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    componentDidMount() {
-    }
+  componentDidMount() {}
 
-    render() {
-        console.log(this.props.data);
-        let data = convertTwoArrToOneArray(this.props.data);
-        console.log(data);
-        return (
-            <div>
-                <LineChart
-                    size={[1000, 500]}
-                    data={data}
-                />
-            </div>
-        );
-    }
+  render() {
+    console.log(this.props.data);
+    let data = convertTwoArrToOneArray(this.props.data);
+    console.log(data);
+    return (
+      <div>
+        <div
+          style={{ width: "100%", backgroundColor: "#d0e7f2", textAlign: "center", padding: "5px 0" }}
+        >
+          ROE (净资产收益率)
+        </div>
+        <div style={{marginTop: "20px"}}>
+            <LineChart size={[1000, 500]} data={data} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default StandardParameterAnalysis;
