f8a5c06f12bea84eecbd5343531f2711d9275d60
Ben Huang <bhuang@thousandeyes.com>
2018-08-26 19:15:29 -0700

    fixes shareNames empty array bug

--- client/src/components/data-vis/MultiSeriesLineChart.js
+++ client/src/components/data-vis/MultiSeriesLineChart.js
@@ -71,127 +71,133 @@ class MultiSeriesLineChart extends Component {
     createLineChart() {
         let svg = select(this.node);
         let shareNames = Object.keys(this.props.data);
+        if (shareNames.length > 0) {
 
-        // set the dimensions and margins of the graph
-        let margin = { top: 20, right: 20, bottom: 30, left: 50 },
-            width = this.props.size[0] - margin.left - margin.right,
-            height = this.props.size[1] - margin.top - margin.bottom;
+            // set the dimensions and margins of the graph
+            let margin = {top: 20, right: 20, bottom: 30, left: 50},
+                width = this.props.size[0] - margin.left - margin.right,
+                height = this.props.size[1] - margin.top - margin.bottom;
 
-        // append the svg object to the body of the page
-        // appends a 'group' element to 'svg'
-        // moves the 'group' element to the top left margin
-        svg
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            // append the svg object to the body of the page
+            // appends a 'group' element to 'svg'
+            // moves the 'group' element to the top left margin
+            svg
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        // set the ranges
-        let x = scaleTime().range([0, width]);
-        let y = scaleLinear().range([height, 0]);
-        // Scale the domains
-        x.domain(
-            extent(this.props.data[shareNames[0]], function(d) {
-                return d ? d.date : null;
-            })
-        );
-        y.domain(this.props.domain);
-
-        // Add the X Axis
-        let originRatio = Math.abs(this.props.domain[1] - 0) / Math.abs(this.props.domain[1] - this.props.domain[0]);
-        svg
-            .append("g")
-            .attr("transform", "translate(30," + (originRatio * height) + ")")
-            .call(axisBottom(x));
-
-        // Add the Y Axis
-        svg
-            .append("g")
-            .attr("transform", "translate(30,0)")
-            .call(axisLeft(y));
-
-        // add the X gridlines
-        /*svg
-            .append("g")
-            .attr("class", "grid")
-            .attr("transform", "translate(0," + height + ")")
-            .call(
-                make_x_gridlines(x)
-                    .tickSize(-height)
-                    .tickFormat("")
+            // set the ranges
+            let x = scaleTime().range([0, width]);
+            let y = scaleLinear().range([height, 0]);
+            // Scale the domains
+            x.domain(
+                extent(this.props.data[shareNames[0]], function (d) {
+                    return d ? d.date : null;
+                })
             );
-        // add the Y gridlines
-        svg
-            .append("g")
-            .attr("class", "grid")
-            .call(
-                make_y_gridlines(y)
-                    .tickSize(-width)
-                    .tickFormat("")
-            );*/
+            y.domain(this.props.domain);
 
-        // parse the date / time
-        //let parseTime = timeParse(this.props.timeFormat);
-
-        for (let i=0; i<shareNames.length; i++) {
-            if (i > 4) { // now only draws up to 5 lines
-                break;
-            }
-
-            let data = this.props.data[shareNames[i]];
-
-            // define the line
-            let valueline = line()
-            //.curve(curveBasis)
-                .defined(function(d) {
-                    return d;
-                })
-                .x(function(d) {
-                    return x(d.date);
-                })
-                .y(function(d) {
-                    return y(d.val);
-                });
-
-            // Add the valueline path.
-            let lineClass = "line" + i;
+            // Add the X Axis
+            let originRatio = Math.abs(this.props.domain[1] - 0) / Math.abs(this.props.domain[1] - this.props.domain[0]);
             svg
-                .append("path")
-                .datum(data)
-                .attr("class", lineClass)
-                .attr("d", valueline)
-                .attr("transform", "translate(30, 0)");
+                .append("g")
+                .attr("transform", "translate(30," + (originRatio * height) + ")")
+                .call(axisBottom(x));
 
-            // Add the dots
-            let dotClass = ".dot" + i;
-            let dotAttrClass = "dot" + i;
+            // Add the Y Axis
             svg
-                .selectAll(dotClass)
-                .data(
-                    data.filter(function(d) {
+                .append("g")
+                .attr("transform", "translate(30,0)")
+                .call(axisLeft(y));
+
+            // add the X gridlines
+            /*svg
+                .append("g")
+                .attr("class", "grid")
+                .attr("transform", "translate(0," + height + ")")
+                .call(
+                    make_x_gridlines(x)
+                        .tickSize(-height)
+                        .tickFormat("")
+                );
+            // add the Y gridlines
+            svg
+                .append("g")
+                .attr("class", "grid")
+                .call(
+                    make_y_gridlines(y)
+                        .tickSize(-width)
+                        .tickFormat("")
+                );*/
+
+            // parse the date / time
+            //let parseTime = timeParse(this.props.timeFormat);
+
+            for (let i = 0; i < shareNames.length; i++) {
+                if (i > 4) { // now only draws up to 5 lines
+                    break;
+                }
+
+                let data = this.props.data[shareNames[i]];
+
+                // define the line
+                let valueline = line()
+                //.curve(curveBasis)
+                    .defined(function (d) {
                         return d;
                     })
-                )
-                .enter()
-                .append("circle")
-                .attr("class", dotAttrClass)
-                .attr("cx", valueline.x())
-                .attr("cy", valueline.y())
-                .attr("r", 3.5)
-                .attr("transform", "translate(30, 0)");
-        }
+                    .x(function (d) {
+                        return x(d.date);
+                    })
+                    .y(function (d) {
+                        return y(d.val);
+                    });
 
-        addLegend(svg, shareNames, this.props.size[0]-100);
+                // Add the valueline path.
+                let lineClass = "line" + i;
+                svg
+                    .append("path")
+                    .datum(data)
+                    .attr("class", lineClass)
+                    .attr("d", valueline)
+                    .attr("transform", "translate(30, 0)");
+
+                // Add the dots
+                let dotClass = ".dot" + i;
+                let dotAttrClass = "dot" + i;
+                svg
+                    .selectAll(dotClass)
+                    .data(
+                        data.filter(function (d) {
+                            return d;
+                        })
+                    )
+                    .enter()
+                    .append("circle")
+                    .attr("class", dotAttrClass)
+                    .attr("cx", valueline.x())
+                    .attr("cy", valueline.y())
+                    .attr("r", 3.5)
+                    .attr("transform", "translate(30, 0)");
+            }
+
+            addLegend(svg, shareNames, this.props.size[0] - 100);
+        }
 
     }
 
     render() {
         return (
+            <div>
+            {this.props.data &&
             <svg
                 ref={node => (this.node = node)}
                 width={this.props.size[0]}
                 height={this.props.size[1]}
-            />
+            />}
+            </div>
+
         );
     }
 }
